713f9bccfc9c45f5b9e72660ae3f5d8a453d6470
Anas Abu Farraj <anas.r.abufarraj@gmail.com>
2019-12-09 21:53:26 +0200

    Backup project

--- playground.py
+++ playground.py
@@ -3,29 +3,13 @@
 #  Copyright (c) 2019. Anas Abu Farraj
 # ------------------------------------------------------------------------------
 
-# import sqlite3
-#
-# connection = sqlite3.connect('data.db')
-# cursor = connection.cursor()
-#
-# cursor.execute(
-#     'CREATE TABLE IF NOT EXISTS users (id INT, username TEXT, password TEXT)')
-#
-# connection.commit()
-# connection.close()
+import sqlite3
 
-from flask import Flask, request
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
 
-APP = Flask(__name__)
+cursor.execute(
+    'CREATE TABLE IF NOT EXISTS users (id INT, username TEXT, password TEXT)')
 
-# @APP.route('/')
-# def index():
-#     foo = request.host_url
-#     return f'<p>foo: {foo}</p>'
-#
-# @APP.route('/')
-# def index():
-#     return '<h1>Bad Request</h1>', 400
-#
-# if __name__ == '__main__':
-#     APP.run(debug=True)
+connection.commit()
+connection.close()
